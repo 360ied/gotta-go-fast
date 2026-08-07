f55fa4131196d20801437a4086ce8eabfe03ad64
360ied <19516527+360ied@users.noreply.github.com>
2026-08-07 19:47:13 -0400

    Add initial Nix flake for gotta-go-fast

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A command line utility for practicing typing";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        haskellPackages = pkgs.haskellPackages;
+
+        gotta-go-fast = haskellPackages.callCabal2nix "gotta-go-fast" ./. {};
+      in
+      {
+        packages = {
+          default = gotta-go-fast;
+          gotta-go-fast = gotta-go-fast;
+        };
+
+        apps = {
+          default = flake-utils.lib.mkApp {
+            drv = gotta-go-fast;
+          };
+          gotta-go-fast = flake-utils.lib.mkApp {
+            drv = gotta-go-fast;
+          };
+        };
+
+        devShells.default = haskellPackages.shellFor {
+          packages = p: [ gotta-go-fast ];
+          buildInputs = with haskellPackages; [
+            cabal-install
+            haskell-language-server
+            ghcid
+          ];
+          withHoogle = true;
+        };
+      });
+}
